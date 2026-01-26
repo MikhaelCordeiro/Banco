71c1eb846284d6ffc03e13b68703c54341879cee
MikhaelCordeiro <mikhacordeiro@gmail.com>
2026-01-26 11:44:51 -0300

    Function to create accounts

---
+++ backend_py/core/contas.py
@@ -0,0 +1,50 @@
+import random
+
+from backend_py.storage.database import supabase
+from backend_py.utils import security
+
+
+def validar_cpf(cpf: str):
+    uniqueValue = supabase.table("contas").select("*").eq("cpf", cpf).execute()
+    print(f"O banco retornou: {uniqueValue.data}")
+    if uniqueValue.data == []:
+        return True
+    else:
+        return False
+
+
+def gerar_numero_conta():
+    while True:
+        numero = random.randint(100000, 999999)
+
+        response = supabase.table("contas").select("*").eq("numero", numero).execute()
+
+        if not response.data:
+            return numero
+
+
+def criar_conta(cpf: str, senha: str):
+    if not validar_cpf(cpf):
+        return "CPF já esta em uso"
+
+    if not security.verificar_forca(senha):
+        return "Senha não segue as Regras"
+
+    meu_salt = security.salt()
+    hash_final = security.hash_senha(senha, meu_salt)
+    numero_final = gerar_numero_conta()
+
+    nova_conta = {
+        "numero": numero_final,
+        "senha": hash_final,
+        "salt": meu_salt,
+        "cpf": cpf,
+    }
+
+    try:
+        return supabase.table("contas").insert(nova_conta).execute()
+    except Exception as e:
+        return f"Erro ao criar conta: {e}"
+
+
+print(criar_conta("135.394.874-82", "SenhaForte123"))
--- backend_py/storage/database.py
+++ backend_py/storage/database.py
@@ -1,16 +1,11 @@
 import os
 
 from dotenv import load_dotenv
-from supabase import create_client
+from supabase import Client, create_client
 
 load_dotenv()
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 
-
-def connect():
-    try:
-        return create_client(url, key)
-    except Exception as e:
-        print(e)
+supabase: Client = create_client(url, key)
