66611a5e662ee25e831ad518c0710722cb74b302
MikhaelCordeiro <mikhacordeiro@gmail.com>
2026-01-25 23:23:48 -0300

    Conexão DB

---
+++ backend_py/storage/database.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+from supabase import create_client
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+
+def connect():
+    try:
+        return create_client(url, key)
+    except Exception as e:
+        print(e)
