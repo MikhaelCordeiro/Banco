75550b744447f2f7bf04618c9cfbb5eb7160ea4e
mateuscordeiro0818 <mateuscordeiro777@gmail.com>
2026-01-26 21:24:03 -0300

    tela

---
+++ frontend_py/src/cli/tela_inicial.py
@@ -0,0 +1,44 @@
+import time
+import getpass
+import sys
+import os
+
+def tela_inicial():
+    print('Seja Bem-Vindo ao PyBank!')
+
+    print('1.Criar Conta PyBank\n2.Já Tenho Conta PyBank')
+
+def escolha():
+    escolher = input('Digite (1 ou 2): ')
+    if escolher == '1':
+        cadastro1()
+    elif escolher == '2':
+        logi()
+    else:
+        print('Error, tente novamente')
+        return False
+
+
+def cadastro1():
+    cpf = input('Digite Seu Cpf: ')
+    senha = getpass.getpass('Digite Sua Senha(Somente Numerais): ')
+    time.sleep(1)
+    print('Cadastro feito com sucesso!')
+    return True
+
+def logi():
+    login_cpf = input('Digite Seu Cpf: ')
+    login_senha = getpass.getpass('Digite Sua Senha: ')
+    print('Login Concluido!')
+    return True
+
+
+def main():
+    while True:
+        tela_inicial()
+        resultado = escolha()
+        if resultado == True:
+            break
+if __name__=='__main__':
+    main()
+        
